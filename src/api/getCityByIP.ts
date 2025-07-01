3012a2e671adbce6688af098e61d230f75989e31
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-07-01 19:25:20 +0100

    refactor IP API error handling

--- src/api/getCityByIP.ts
+++ src/api/getCityByIP.ts
@@ -1,21 +1,16 @@
 export default async function getCityByIP(): Promise<string> {
   const res = await fetch('https://ip-api.io/json/');
 
+  const statusMessages: Record<number, string> = {
+    404: 'IP address not found',
+    429: "Couldn't find your city automatically - Please try manual search",
+    500: 'Internal server error',
+  };
+
   if (!res.ok) {
-    if (res.status === 404) {
-      throw new Error('IP address not found');
-    }
-
-    if (res.status === 429) {
-      throw new Error(
-        "Couldn't find your city automatically - Please try manual search"
-      );
-    }
-
-    if (res.status === 500) {
-      throw new Error('Internal server error');
-    }
-    throw new Error(`Unexpected error: ${res.status}`);
+    const errorMessage =
+      statusMessages[res.status] || `Unexpected error: ${res.status}`;
+    throw new Error(errorMessage);
   }
 
   const data = await res.json();

b452b65522c814a39b353773b46ca4d259b639f1
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-07-01 19:17:53 +0100

    improved error handling for automatic city detection

--- src/api/getCityByIP.ts
+++ src/api/getCityByIP.ts
@@ -1,5 +1,23 @@
 export default async function getCityByIP(): Promise<string> {
   const res = await fetch('https://ip-api.io/json/');
+
+  if (!res.ok) {
+    if (res.status === 404) {
+      throw new Error('IP address not found');
+    }
+
+    if (res.status === 429) {
+      throw new Error(
+        "Couldn't find your city automatically - Please try manual search"
+      );
+    }
+
+    if (res.status === 500) {
+      throw new Error('Internal server error');
+    }
+    throw new Error(`Unexpected error: ${res.status}`);
+  }
+
   const data = await res.json();
 
   if (data && data.city) {
